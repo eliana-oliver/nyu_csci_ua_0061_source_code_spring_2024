604e0a9b416febb79aebfa461312f2614e961221
eliana-oliver <157847832+eliana-oliver@users.noreply.github.com>
2024-02-15 15:29:35 -0500

    Update 04_ProgrammingChallenge.js

--- days_07_08/01-DOM_classes_selecting_multiple_elements_and_creating_new_elements/04_ProgrammingChallenge.js
+++ days_07_08/01-DOM_classes_selecting_multiple_elements_and_creating_new_elements/04_ProgrammingChallenge.js
@@ -13,13 +13,42 @@ const contentGlazed = document.getElementById('contentGlazed');
 //  event handler to make our Welcome Tab active
 tabWelcome.onclick = function() {
   // first, make this tab our 'active' tab by applying the 'active' class to it
-
+  tabWelcome.classList.add("active");
   // remove the 'active' class from the other tabs
-
+  tabTraditional.classList.remove("active");
+  tabGlazed.classList.remove("active");
   // show the content associated with this tab
-
+  contentWelcome.classList.remove("hidden");
   // hide the other two tabs
-
+  contentTraditional.classList.add("hidden");
+  contentGlazed.classList.add("hidden");
 }
+
+tabTraditional.onclick = function() {
+  // first, make this tab our 'active' tab by applying the 'active' class to it
+  tabTraditional.classList.add("active");
+  // remove the 'active' class from the other tabs
+  tabWelcome.classList.remove("active");
+  tabGlazed.classList.remove("active");
+  // show the content associated with this tab
+  contentTraditional.classList.remove("hidden");
+  // hide the other two tabs
+  contentWelcome.classList.add("hidden");
+  contentGlazed.classList.add("hidden");
+}
+
+tabGlazed.onclick = function() {
+  // first, make this tab our 'active' tab by applying the 'active' class to it
+  tabGlazed.classList.add("active");
+  // remove the 'active' class from the other tabs
+  tabTraditional.classList.remove("active");
+  tabWelcome.classList.remove("active");
+  // show the content associated with this tab
+  contentGlazed.classList.remove("hidden");
+  // hide the other two tabs
+  contentTraditional.classList.add("hidden");
+  contentWelcome.classList.add("hidden");
+}
+
 
 
